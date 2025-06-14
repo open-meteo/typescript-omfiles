00b2ef998371acaee938bbb08f8fa77aa2a17e9e
terraputix <terraputix@mailbox.org>
2025-06-14 11:33:49 +0200

    rename file

--- packages/file-reader/src/index.ts
+++ packages/file-reader/src/index.ts
@@ -1,6 +1,6 @@
 export { OmFileReader } from "./lib/OmFileReader";
 export { FileBackend } from "./lib/backends/FileBackend";
-export { MemoryHttpBackend } from "./lib/backends/MemoryS3Backend";
+export { MemoryHttpBackend } from "./lib/backends/MemoryHttpBackend";
 export { OmFileReaderBackend } from "./lib/backends/OmFileReaderBackend";
 export { initWasm, getWasmModule } from "./lib/wasm";
 export * from "./lib/types";
