afa74ecbe7278466840d68c039d8e1b6f3915c9b
terraputix <terraputix@mailbox.org>
2026-02-12 13:24:06 +0100

    feat: support AbortSignal

--- packages/file-reader/src/lib/backends/OmFileReaderBackend.ts
+++ packages/file-reader/src/lib/backends/OmFileReaderBackend.ts
@@ -4,20 +4,25 @@ export interface OmFileReaderBackend {
    * Get bytes from the backend at the specified offset and size
    * @param offset The offset in bytes from the start of the file
    * @param size The number of bytes to read
+   * @param signal Optional AbortSignal to cancel the operation
    */
-  getBytes(offset: number, size: number): Promise<Uint8Array>;
+  getBytes(offset: number, size: number, signal?: AbortSignal): Promise<Uint8Array>;
 
   /**
    * Collects prefetch tasks for a given range without executing them.
    * Returns an array of functions that, when called, will fetch the data.
    * This allows the caller to control concurrency.
+   * @param offset The offset in bytes from the start of the file
+   * @param size The number of bytes to prefetch
+   * @param signal Optional AbortSignal to cancel the operation
    */
-  collectPrefetchTasks?(offset: number, size: number): Promise<Array<() => Promise<void>>>;
+  collectPrefetchTasks?(offset: number, size: number, signal?: AbortSignal): Promise<Array<() => Promise<void>>>;
 
   /**
    * Get the total size of the file in bytes
+   * @param signal Optional AbortSignal to cancel the operation
    */
-  count(): Promise<number>;
+  count(signal?: AbortSignal): Promise<number>;
 
   /**
    * Close the backend and release any resources
