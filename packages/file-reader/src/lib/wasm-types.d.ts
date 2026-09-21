3ea49228c74e2cdb8d69e35c7ce7d7fe93a34f6e
Vincent van der Wal <github@vincentvanderwal.nl>
2026-09-21 12:17:55 +0200

    rename to om_file_format

--- packages/file-reader/src/lib/wasm-types.d.ts
+++ packages/file-reader/src/lib/wasm-types.d.ts
@@ -1,4 +1,4 @@
-declare module "*om_reader.js" {
+declare module "*om_file_format.js" {
   // This default export function creates the module
   function ModuleFactory(options?: {
     locateFile?: (path: string) => string;
